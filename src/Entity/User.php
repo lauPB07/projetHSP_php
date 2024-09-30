ffffb2badcff029e903d8574e830913f019ee8d6
Magne Rayan <r.magne@lprs.fr>
2024-09-30 14:55:15 +0200

    feat: creation de l'inscription d'utilisateur

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface
 {
     #[ORM\Id]
@@ -97,6 +99,9 @@ class User implements UserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $formationEtudiant = null;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
 
 
 
@@ -483,5 +488,17 @@ class User implements UserInterface
     {
         return $this->email;
         // TODO: Implement getUserIdentifier() method.
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
     }
 }
