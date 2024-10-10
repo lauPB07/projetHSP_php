144d6278805ddf78443e539ba6ac38200b41d110
lauPB07 <laurapiresberclaz@gmail.com>
2024-10-10 10:37:34 +0200

    fix : modification de la header bar par rapport a chaque role, il manque la partie admin elle est en cour

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -423,15 +423,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        // Commencez par un tableau vide de rôles
         $roles = [];
 
-        // Si l'utilisateur a un rôle, ajoutez son nom au tableau
         if ($this->ref_role) {
-            $roles[] = $this->ref_role->getNomRole(); // Supposons que Role a une méthode getName()
+            $roles[] = $this->ref_role->getNomRole();
         }
 
-        // Retournez le tableau des rôles
         return $roles;
     }
 
