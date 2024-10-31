b172763c66cf3eb54078eddd192daa61c7fa8a5f
djibr <d.korchi@lprs.fr>
2024-10-31 13:49:06 +0100

    Modification de la bdd en changeant la ref_admin 'ref_question_id' de la table user a la table question_support création du crud post, modification du fichier index_question celon le compte et mise en place du bouton forum pour tous les utilisateur

--- src/Controller/QuestionSupportController.php
+++ src/Controller/QuestionSupportController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\QuestionSupport;
-use App\Entity\ReponseSupport;
 use App\Form\QuestionSupportType;
+use App\Form\ReponseSupportType;
 use App\Repository\QuestionSupportRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/question/support')]
 final class QuestionSupportController extends AbstractController
 {
-    #[Route(name: 'app_question_support_index', methods: ['GET'])]
+    #[Route('/',name: 'app_question_support_index', methods: ['GET'])]
     public function index(QuestionSupportRepository $questionSupportRepository): Response
     {
         return $this->render('question_support/index.html.twig', [
@@ -68,14 +68,27 @@ final class QuestionSupportController extends AbstractController
             'form' => $form,
         ]);
     }
+    #[Route('/{id}/edit', name: 'app_reponse_support_edit', methods: ['GET', 'POST'])]
+    public function reponse(Request $request, QuestionSupport $questionSupport, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ReponseSupportType::class, $questionSupport);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reponse_support_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('question_support/reponse.html.twig', [
+            'question_support' => $questionSupport,
+            'form' => $form,
+        ]);
+    }
 
     #[Route('/{id}', name: 'app_question_support_delete', methods: ['POST'])]
-    public function delete(Request $request, QuestionSupport $questionSupport,ReponseSupport $reponseSupport, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, QuestionSupport $questionSupport, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$reponseSupport->getRefQuestion(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($reponseSupport);
-            $entityManager->flush();
-        }
         if ($this->isCsrfTokenValid('delete'.$questionSupport->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($questionSupport);
             $entityManager->flush();
