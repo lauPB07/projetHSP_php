9064ad4f4ec930db4181334214c8694d60eff1b8
Magne Rayan <r.magne@lprs.fr>
2024-12-09 14:08:11 +0100

    fix : conflit indexEtudiant.html et HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,30 +14,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(Request $name): Response
     {
-        $mj = new Client($_ENV['MJ_APIKEY_PUBLIC'], $_ENV['MJ_APIKEY_PRIVATE'],true,['version' => 'v3.1']);
-        $body = [
-            'Messages' => [
-                [
-                    'From' => [
-                        'Email' => "projethsp0@gmail.com",
-                        'Name' => "Me"
-                    ],
-                    'To' => [
-                        [
-                            'Email' => "djibrilkorchi@yopmail.com",
-                            'Name' => "You"
-                        ]
-                    ],
-                    'Subject' => "My first Mailjet Email!",
-                    'TextPart' => "Greetings from Mailjet!",
-                    'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3>
-            <br />May the delivery force be with you!"
-                ]
-            ]
-        ];
-        $response = $mj->post(Resources::$Email, ['body' => $body]);
-
-        $response->success() && var_dump($response->getData());
         return $this->render('home/index.html.twig');
     }
 }
