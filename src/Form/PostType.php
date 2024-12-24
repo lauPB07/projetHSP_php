e13352985ee96517c3d375013beaf1a9f4dd3ed2
lauPB07 <laurapiresberclaz@gmail.com>
2024-12-24 10:39:40 +0100

    fix : modification du css pour le forum

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,7 +6,10 @@ use App\Entity\Post;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,14 +18,40 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('canal')
-            ->add('titre')
-            ->add('contenue')
-            ->add('date', null, [
-                'widget' => 'single_text'
+            ->add('canal', TextType::class, [
+                'label' => 'Canal',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez le canal'
+                ],
             ])
-            ->add('Save', SubmitType::class)
-        ;
+            ->add('titre', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez le titre'
+                ],
+            ])
+            ->add('contenue', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Rédigez le contenu'
+                ],
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('Save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-primary mt-3',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
