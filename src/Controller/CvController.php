fc173e34b73c47af6905b82404aff3b001a2948e
djibr <d.korchi@lprs.fr>
2024-12-27 19:06:48 +0100

    Feat: suprimer des utilisateur via l'anuaire pour l'admin, le bouton télécharge cv des étudiant qui ne marche pas

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace App\Controller;
-
-class CvController
-{
-
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class CvController extends AbstractController{
+    #[Route('/download-cv/{id}', name: 'download_cv')]
+    public function downloadCv(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+        // Récupérer le contenu du CV
+        $cvContent = $user->getCv();
+        $fileName = 'cv_etudiant_' . $id . '.pdf'; // Par exemple, un nom générique
+        // Retourner une réponse de téléchargement
+        return new Response(
+            $cvContent,
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="' . $fileName . '"',
+            ]
+        );
+    }
 }
